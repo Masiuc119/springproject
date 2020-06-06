39237d6f0e39c1a7d0395d2366bd75c3f209ba58
Alex <masiuc1.19@gmail.com>
2020-06-06 14:44:43 +0300

    Spring Задание 1

---
+++ src/test/java/by/masiuk/springproject/SpringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package by.masiuk.springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
